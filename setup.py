f99bc5e25133c140ca656a179a2eb695f4708f55
23subbhashit <subbhashitmukherjee@gmail.com>
2023-09-29 23:53:24 +0530

    Tested with Basic  Operations

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MLOPS-IIITB",
-    version="0.0.1",
+    version="0.0.2",
     author="subbhashit mukherjee",
     author_email="subbhashitmukherjee@gmail.com",
     packages=["MLOX"],
